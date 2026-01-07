ea6c8eb1086a4330220476daad16595a86042e41
CaioQC <caioq2004@gmail.com>
2026-01-07 17:13:16 -0300

    Add query method with multiple criteria

--- src/main/java/com/workshopmongo/dto/CommentDTO.java
+++ src/main/java/com/workshopmongo/dto/CommentDTO.java
@@ -3,30 +3,29 @@ package com.workshopmongo.dto;
 import java.io.Serializable;
 import java.util.Date;
 
-public class CommentDTO implements Serializable{
+public class CommentDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private String id;
+
+	private String text;
 	private Date date;
 	private AuthorDTO author;
 	
 	public CommentDTO() {
-		
 	}
 
-	public CommentDTO(String id, Date date, AuthorDTO author) {
+	public CommentDTO(String text, Date date, AuthorDTO author) {
 		super();
-		this.id = id;
+		this.text = text;
 		this.date = date;
 		this.author = author;
 	}
 
-	public String getId() {
-		return id;
+	public String getText() {
+		return text;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setText(String text) {
+		this.text = text;
 	}
 
 	public Date getDate() {
